25e572946cb3316fe348bfcc21407fe24b0a2e04
Antichess <cahalng@gmail.com>
2023-01-27 22:30:01 -0500

    initial tab

--- pyqt/main_window.py
+++ pyqt/main_window.py
@@ -7,11 +7,30 @@ class mainObj(QWidget):
         pass
     
     def layout(self):
-        self.label = QLabel("Hello world!")
-        self.w_layout = QVBoxLayout()
-        self.w_layout.addWidget(self.label)
+        self.tab_layout = QTabWidget()
+
+        #TAB 1
+        self.tab1_layout = QVBoxLayout()
+        self.tab1_layout.addWidget(QLabel("Hello World!"))
+
+        self.tab1 = QWidget()
+        self.tab1.setLayout(self.tab1_layout)
+
+        #TAB 2
+        self.tab2_layout = QVBoxLayout()
+        self.camera = App()
+        self.tab2_layout.addWidget(self.camera)
+
+        self.tab2 = QWidget()
+        self.tab2.setLayout(self.tab2_layout)
+
+        self.tab_layout.addTab(self.tab1, "Text")
+        self.tab_layout.addTab(self.tab2, "Camera")
+        
+        self.final_qv_tab_layout = QVBoxLayout()
+        self.final_qv_tab_layout.addWidget(self.tab_layout)
 
         self.w = QWidget()
-        self.w.setLayout(self.w_layout)
+        self.w.setLayout(self.final_qv_tab_layout)
         
         return self.w
